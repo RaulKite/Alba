c585e53066335b2fb54511bd0f1ba1dbfb99bf49
Raul Sanchez <raul@um.es>
2012-02-15 10:25:16 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Alba::Application.config.secret_token = 'fbf37048d26a7f312d0eb70c5999a9888abe4c8b3d49f0c479437abde373f76d786765e4c0222d90a5f58bb18e3aa38c15daba48335bf8da52243505f89555b4'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Alba::Application.config.session_store :cookie_store, key: '_alba_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Alba::Application.config.session_store :active_record_store
